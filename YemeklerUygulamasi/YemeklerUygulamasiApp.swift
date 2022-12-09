360aa060f0fa57689f2b059b54f61fc6a9c6a34b
ilkerugurkaya <kayailkerugur@gmail.com>
2022-12-09 14:46:35 +0300

    Initial Commit

---
+++ YemeklerUygulamasi/YemeklerUygulamasiApp.swift
@@ -0,0 +1,17 @@
+//
+//  YemeklerUygulamasiApp.swift
+//  YemeklerUygulamasi
+//
+//  Created by İlker Kaya on 9.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct YemeklerUygulamasiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
